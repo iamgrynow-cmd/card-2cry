bc8022c2e4257fb4bcf8eb8fb525cd68a202ceb4
iamgrynow-cmd <iamgrynow@gmail.com>
2026-04-25 15:20:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,33 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendEmailVerification,
-  updateProfile,
-  signOut,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGwpH-JYuYK4kkUpf3cCsp-Im4urQW9Lk",
-  authDomain: "nexpay-crypto-card.firebaseapp.com",
-  projectId: "nexpay-crypto-card",
-  storageBucket: "nexpay-crypto-card.firebasestorage.app",
-  messagingSenderId: "120822872109",
-  appId: "1:120822872109:web:9079b4cf4f0cade9c620a4",
-  measurementId: "G-KQE5VRLX4F"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export {
-  auth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendEmailVerification,
-  updateProfile,
-  signOut,
-  onAuthStateChanged
-};
+export const app = initializeApp(firebaseConfig);
